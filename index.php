8f52b74fc9a9a56d5ebba37dbded53d98e768ac1
Bruno Filipe Ramos Martins <brunofiliperamos@hotmail.com>
2019-05-21 17:18:59 -0300

    Ajustes

--- index.php
+++ index.php
@@ -6,43 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     <!-- Bootstrap -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <!-- Slick Carousel -->
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <div class="one-time">
-            
+            <?php
+                // $url = 'https://jsonplaceholder.typicode.com/photos';
+                $data = file_get_contents($url);
+                $characters = json_decode($data);
+                
+                foreach ($characters as $character) {
+            ?>
+                    <div>
+                        <img src="<?php echo $character->thumbnailUrl ?>" alt="">
+                        <a href="<?php echo $character->url ?>"></a>
+                    </div>
+            <?php
+                }
+            ?>
         </div>
-        <?php
-            $url = 'https://jsonplaceholder.typicode.com/photos';
-            $data = file_get_contents($url);
-            $characters = json_decode($data);
-            
-            foreach ($characters as $character) {
-        ?>
-                <div>
-                    <img src="<?php echo $character->thumbnailUrl ?>" alt="">
-                    <a href="<?php echo $character->url ?>"></a>
-                </div>
-        <?php
-            }
-        ?>
         <h1>asdasd</h1>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
     <script>
-        $('.one-time').slick({
-            dots: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            adaptiveHeight: true
+        $(document).ready(function () {
+            $('.one-time').slick({
+                dots: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                adaptiveHeight: true
+            });
         });
     </script>
 </body>
